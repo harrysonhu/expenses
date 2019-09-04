080ae46ef8d5db6d52bd023e3919de3d800ec15d
Harryson Hu <harrysonhu@fingerfoodstudios.com>
2019-09-03 22:22:10 -0700

    Add material-ui as dependency, do a sample button to make sure it installed properly

--- src/components/MainPage/MainPage.jsx
+++ src/components/MainPage/MainPage.jsx
@@ -1,3 +1,11 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
-export const MainPage = () => <h1>How much are you spending?</h1>
+export const MainPage = () => (
+    <>
+        <h1>How much are you spending?</h1>
+        <Button variant="contained" color="secondary">
+            Click Here!
+        </Button>
+    </>
+)
